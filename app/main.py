c22e2c4578ed9fa39b33e91fe5e7ecfcc9d94da5
Angus <angus.goody@gmail.com>
2025-01-07 23:47:24 +0000

    tweaks to main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from app.facades.log_facade import LogFacade
+from app.repositories.mount_repository import MountRepository
+from app.services.mounting_service import MountingService
 from app.util.config import ConfigManager
 from app.util.message import MESSAGE
 
@@ -19,7 +21,14 @@ def main():
     config_manager.load_from_env()
     LogFacade.info(config_manager)
 
-    #
+    # Create a mount repository
+    mount_repository = MountRepository(config_manager)
+
+    # Pass the mount repository to the mounting service
+    mounting_service = MountingService(mount_repository)
+
+    # Run the mounting service
+    mounting_service.run()
 
 
 if __name__ == "__main__":
